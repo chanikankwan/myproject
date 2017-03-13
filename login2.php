c148791d4fb52c77adb4bd1615234c47520e0d81
chanikan seerat <chanikan-st@rmutsb.ac.th>
2017-03-13 11:41:05 +0700

    myproject

---
+++ login2.php
@@ -0,0 +1,10 @@
+<?php
+if ($_POST['username']=="chanikan" && $_POST['password']=="1111") {
+  echo"ยินดีต้อนรับคุณ".$_POST['username'];
+  echo"<a href='http://www.huntra.bait.rmutsb.ac.th> BIT Site </a>";
+  }
+else {
+  echo"คุณป้อนข้อมูลไม่ถูกต้อง";
+  echo"<a href='loginform.php'>กลับหน้า Loginform </a>";
+  }
+?>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+echo  "php hello world <br> ";
+echo "chanikan <br> ";
+echo "i am web programmer <br> <hr> ";
+?>
